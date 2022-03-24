83d07d6c612e7744084959877367d9aebf4dc069
Victor Inojosa <vijoin@gmail.com>
2022-03-24 10:17:11 -0300

    Update GovernorContract according to OpenZeppeing wizard

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
-    uint256 public s_maxSupply = 1000000000000000000000000;
+    uint256 public s_maxSupply = 1000000000000000000000000; // 1M
 
     constructor() ERC20("GovernanceToken", "GT") ERC20Permit("GovernanceToken") {
         _mint(msg.sender, s_maxSupply);
--- contracts/governance_standard/GovernorContract.sol
+++ contracts/governance_standard/GovernorContract.sol
@@ -1,48 +1,52 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 
-
-contract GovernorContract is 
-    Governor,
-    GovernorCountingSimple,
-    GovernorVotes,
-    GovernorVotesQuorumFraction,
-    GovernorTimelockControl
-{
-    uint256 public s_votingDelay;
-    uint256 public s_votingPeriod;
-
-    constructor (
-        ERC20Votes _token,
+contract GovernorContract is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
+    constructor(
+        IVotes _token,
         TimelockController _timelock,
-        uint256 _quorumPercentage,
+        uint256 _votingDelay,
         uint256 _votingPeriod,
-        uint256 _votingDelay
+        uint256 _quorumPercentage
     )
         Governor("GovernorContract")
+        GovernorSettings(
+            _votingDelay, /* 1 block ~ 13.2 sec */
+            _votingPeriod, /* 45818 blocks ~1 week */
+            0
+        )
         GovernorVotes(_token)
+        GovernorVotesQuorumFraction(_quorumPercentage)
         GovernorTimelockControl(_timelock)
-    {
-        s_votingDelay = _votingDelay;
-        s_votingPeriod = _votingPeriod;
-    }
-
-    function votingDelay() public view override returns (uint256) {
-        return s_votingPeriod; // 1 = 1 block
-    }
-
-    function votingPeriod() public view override returns (uint256) {
-        return s_votingPeriod; // 45818 = 1 week
-    }
+    {}
 
     // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
+
     function quorum(uint256 blockNumber)
         public
         view
@@ -70,35 +74,44 @@ contract GovernorContract is
         return super.state(proposalId);
     }
 
-    function propose(
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        string memory description
-    ) public override(Governor, IGovernor) returns (uint256) {
+    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description)
+        public
+        override(Governor, IGovernor)
+        returns (uint256)
+    {
         return super.propose(targets, values, calldatas, description);
     }
 
-    function _execute(
-        uint256 proposalId,
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        bytes32 descriptionHash
-    ) internal override(Governor, GovernorTimelockControl) {
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+
+    function _execute(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal
+        override(Governor, GovernorTimelockControl)
+    {
         super._execute(proposalId, targets, values, calldatas, descriptionHash);
     }
 
-    function _cancel(
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        bytes32 descriptionHash
-    ) internal override(Governor, GovernorTimelockControl) returns (uint256) {
+    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal
+        override(Governor, GovernorTimelockControl)
+        returns (uint256)
+    {
         return super._cancel(targets, values, calldatas, descriptionHash);
     }
 
-    function _executor() internal view override(Governor, GovernorTimelockControl) returns (address) {
+    function _executor()
+        internal
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (address)
+    {
         return super._executor();
     }
 
